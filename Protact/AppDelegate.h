c0003efa29708f4a920a6eaafb31c78738148ad7
Ryan Lindbeck <rycharlind@Ryans-iMac.local>
2013-11-07 19:28:51 -0600

    Initial Commit

---
+++ Protact/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Protact
+//
+//  Created by Ryan Lindbeck on 11/7/13.
+//  Copyright (c) 2013 Inndevers LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Protact/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Protact
+//
+//  Created by Ryan Lindbeck on 11/7/13.
+//  Copyright (c) 2013 Inndevers LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
